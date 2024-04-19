28d477f5bcbf1c5d376cf9def0168b2817cc880f
TanyRM <tanielemadu@outlook.com>
2024-04-19 16:36:37 -0300

    Classe PesssoaFisica

---
+++ atividade07/PessoaFisica.java
@@ -0,0 +1,11 @@
+package atividade07;
+
+import java.util.Date;
+import java.util.List;
+
+public class PessoaFisica {
+    private String nome;
+    private String sexo;
+    private Date dataNascimento;
+    private List<Endereco> enderecos;
+}
