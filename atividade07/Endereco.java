5a413089505438dbb9b5d4f8692ea00cd667a20d
TanyRM <tanielemadu@outlook.com>
2024-04-19 16:33:13 -0300

    Classe Endereco

---
+++ atividade07/Endereco.java
@@ -0,0 +1,13 @@
+package atividade07;
+
+import java.util.List;
+
+public class Endereco {
+    private Integer numero;
+    private String complemento;
+    private Integer CEP;
+    private Bairro bairro;
+    private TipoEndereco tipo;
+    private Logradouro logradouro;
+    private List<PessoaFisica> moradores;
+}
