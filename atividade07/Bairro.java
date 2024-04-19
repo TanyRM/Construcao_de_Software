b2d6cff68e1dba272eeeb16eec48418aec140aa1
TanyRM <tanielemadu@outlook.com>
2024-04-19 16:28:05 -0300

    Classes Cidade e Bairro

---
+++ atividade07/Bairro.java
@@ -0,0 +1,6 @@
+package atividade07;
+
+public class Bairro {
+    private String nome;
+    public Cidade cidade;
+}
---
+++ atividade07/Cidade.java
@@ -0,0 +1,9 @@
+package atividade07;
+
+import java.util.List;
+
+public class Cidade {
+    private String nome;
+    public Estado estado;
+    public List<Bairro> bairros;
+}
