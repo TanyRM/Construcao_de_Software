18f31849d943db048f01bc33930764ebe20f7e83
TanyRM <tanielemadu@outlook.com>
2024-04-19 16:25:03 -0300

    Classes Estado e Pais

---
+++ atividade07/Estado.java
@@ -0,0 +1,10 @@
+package atividade07;
+
+import java.util.List;
+
+public class Estado {
+    private String sigla;
+    public String nome;
+    public Pais pais;
+    public List<Cidade> cidades;
+}
---
+++ atividade07/Pais.java
@@ -0,0 +1,8 @@
+package atividade07;
+
+import java.util.List;
+
+public class Pais {
+    public String nome;
+    public List<Estado> estados;
+}
