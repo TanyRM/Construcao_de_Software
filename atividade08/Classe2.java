f372deb04db98b626a448621e2cec3345035ab37
TanyRM <tanielemadu@outlook.com>
2024-04-25 16:43:43 -0300

    Classe para questão 2

---
+++ atividade08/Classe2.java
@@ -0,0 +1,12 @@
+package atividade08;
+
+import java.util.InputMismatchException;
+
+public class Classe2 {
+
+    public int[] valores;
+
+    public Classe2() {
+        this.valores = new int[10];
+    }
+}
