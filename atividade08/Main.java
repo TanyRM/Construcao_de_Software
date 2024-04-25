e77c0fc27b7190dc1cf431d2ad0f1135539eac07
TanyRM <tanielemadu@outlook.com>
2024-04-25 16:12:52 -0300

    Main para questão 1

---
+++ atividade08/Main.java
@@ -0,0 +1,21 @@
+package atividade08;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Classe1 classe1 = new Classe1();
+
+        System.out.println("Insira o primeiro numero:");
+        int num1 = scan.nextInt();
+
+        System.out.println("Insira o segundo numero:");
+        int num2 = scan.nextInt();
+
+        System.out.println(classe1.dividir(num1, num2));
+
+    }
+}
