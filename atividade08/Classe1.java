8824db80c45c0ea61fe55e5476ea7612b86e85cc
TanyRM <tanielemadu@outlook.com>
2024-04-26 16:14:52 -0300

    Correção da questão 1

--- atividade08/Classe1.java
+++ atividade08/Classe1.java
@@ -8,13 +8,7 @@ public class Classe1 {
 
         if (num2 == 0) {
             throw new ArithmeticException("Divisão por zero não é permitida.");
-        } else if (!(eInt(num1)) || !(eInt(num2))) {
-            throw new InputMismatchException("Valor informado não é numérico.");
         }
         return num1 / num2;
-    }
-
-    private boolean eInt(int valor){
-        return valor >= Integer.MIN_VALUE && valor <= Integer.MAX_VALUE;
     }
 }
--- atividade08/Main1.java
+++ atividade08/Main1.java
@@ -8,10 +8,9 @@ public class Main1 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        Classe1 classe1 = new Classe1();
 
         try {
-            Classe1 classe1 = new Classe1();
-
             System.out.println("Insira o primeiro numero:");
             int num1 = scan.nextInt();
 
@@ -19,10 +18,9 @@ public class Main1 {
             int num2 = scan.nextInt();
 
             System.out.println(classe1.dividir(num1, num2));
-        } catch (ArithmeticException a) {
-            System.out.println(a.getMessage());
+
         } catch (InputMismatchException e) {
-            System.out.println(e.getMessage());
+            System.out.println("O valor informado não é numérico");
         }
     }
 }
