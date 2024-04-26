2f0954236d797bed7ac4f0475358d627b8c80f1d
TanyRM <tanielemadu@outlook.com>
2024-04-26 16:45:24 -0300

    Correções no codigo

--- atividade08/Classe1.java
+++ atividade08/Classe1.java
@@ -7,7 +7,7 @@ public class Classe1 {
     public int dividir(int num1, int num2) throws ArithmeticException, InputMismatchException {
 
         if (num2 == 0) {
-            throw new ArithmeticException("Divisão por zero não é permitida.");
+            throw new ArithmeticException();
         }
         return num1 / num2;
     }
--- atividade08/Main3.java
+++ atividade08/Main3.java
@@ -3,9 +3,9 @@ package atividade08;
 public class Main3 {
     public static void main(String[] args) throws LoginInvalidoException {
 
-        try {
-            Login login = new Login("Taniele", "123456");
+        Login login = new Login("Taniele", "123456");
 
+        try {
             System.out.println(login.fazerLogin("Taniele", "123456"));
 
             System.out.println(login.fazerLogin("Taniele", "12345"));
